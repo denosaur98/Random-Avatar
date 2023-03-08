8a05495d45706a4befc9555f59b4d833eaf58bb2
denosaur98 <densichurchill1998@gmail.com>
2023-03-08 19:14:58 +0300

    backgrounds_done

--- src/components/backgrounds/BackgroundImg.vue
+++ src/components/backgrounds/BackgroundImg.vue
@@ -1,40 +1,39 @@
 <template>
-    <div class="background_img">
-      <img :src="selectedImg" :class="imgClass" />
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        selectedImg: require("../backgrounds/Boom.png"),
-        imgClass: "background_imgs",
-        images: [
-          { src: require("../backgrounds/Boom.png"), class: "background_imgs" },
-          { src: require("../backgrounds/Bricks.png"), class: "background_imgs" },
-          { src: require("../backgrounds/Rectangle.png"), class: "background_imgs" },
-        ],
-      };
+  <div class="background_img">
+    <img :src="selectedImg" :class="imgClass" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedImg: require("../backgrounds/Boom.png"),
+      imgClass: "background_imgs",
+      images: [
+        { src: require("../backgrounds/Boom.png"), class: "background_imgs" },
+        { src: require("../backgrounds/Bricks.png"), class: "background_imgs" },
+        { src: require("../backgrounds/Rectangle.png"), class: "background_imgs" },
+      ],
+    };
+  },
+  methods: {
+    random() {
+      const randIndex = Math.floor(Math.random() * this.images.length);
+      this.selectedImg = this.images[randIndex].src;
+      this.imgClass = this.images[randIndex].class;
     },
-    methods: {
-      random() {
-        const randIndex = Math.floor(Math.random() * this.images.length);
-        this.selectedImg = this.images[randIndex].src;
-        this.imgClass = this.images[randIndex].class;
-      },
-    },
-  };
-  </script>
-  
-  <style>
-  .background_img {
-    display: flex;
-  }
-  
-  .background_imgs {
-    width: 440px;
-    height: 440px;
-  }
-  </style>
-  
+  },
+};
+</script>
+
+<style>
+.background_img {
+  display: flex;
+}
+
+.background_imgs {
+  width: 440px;
+  height: 440px;
+}
+</style>
